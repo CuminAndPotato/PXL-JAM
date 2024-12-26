16796f85059e3457ad669ae6c2081442f0d3cf09
Urs Enzler <u.enzler@calitime.ch>
2024-12-26 15:11:12 +0100

    first experiments

---
+++ PxlApps/04_Submissions/ursenzler_GameOfLife.fsx
@@ -0,0 +1,199 @@
+﻿#load "../../.deps/PxlLocalDevShadow.fsx"
+open PxlLocalDevShadow
+
+open System
+open Pxl
+open Pxl.Ui
+
+let numbers =
+    [
+        [
+            "XXXX"
+            "X XX"
+            "XX X"
+            "X  X"
+            "XXXX"
+        ]
+        [
+            "  X "
+            " XX "
+            "  X "
+            "  X "
+            "  X "
+        ]
+        [
+            " XX "
+            "X  X"
+            "  X "
+            " X  "
+            "XXXX"
+        ]
+        [
+            "XXX "
+            "   X"
+            " XXX"
+            "   X"
+            "XXX "
+        ]
+        [
+            "X  X"
+            "X  X"
+            "XXXX"
+            "   X"
+            "   X"
+        ]
+        [
+            "XXXX"
+            "X   "
+            "XXX "
+            "   X"
+            "XXX "
+        ]
+        [
+            " XX "
+            "X   "
+            "XXX "
+            "X  X"
+            " XX "
+        ]
+        [
+            "XXXX"
+            "   X"
+            "  X "
+            "  X "
+            "  X "
+        ]
+        [
+            " XX "
+            "X  X"
+            " XX "
+            "X  X"
+            " XX "
+        ]
+        [
+            " XX "
+            "X  X"
+            " XXX"
+            "   X"
+            " XX "
+        ]
+    ]
+
+let time (now: DateTime) =
+    scene {
+        let! ctx = getCtx()
+
+        let timeText = text.var4x5($"{now:HH}:{now:mm}").color(Colors.white)
+        let textWidth = timeText.measure()
+        let marginLeft = (ctx.width - textWidth) / 2
+        let marginTop = (ctx.height - (int timeText._data.fontSize) - 1) / 2
+        timeText
+            .xy(marginLeft, marginTop)
+            .color(Colors.white.opacity(0.5))
+    }
+
+type World = bool array
+
+let getInitialWorld hours minutes =
+    let drawDigit (digit: string list) (world: World) x y =
+        for r in 0..4 do
+            let line = digit[r]
+            for c in 0..3 do
+                if line[c] = 'X' then
+                    world[(r + y) * 24 + c + x] <- true
+
+    let world = Array.create (24*24) false
+    let h1 = (hours / 10)
+    let h2 = (hours % 10)
+    let m1 = (minutes / 10)
+    let m2 = (minutes % 10)
+    let d1 = numbers[h1]
+    let d2 = numbers[h2]
+    let d3 = numbers[m1]
+    let d4 = numbers[m2]
+
+    drawDigit d1 world 1 9
+    drawDigit d2 world 6 9
+    drawDigit d3 world 13 9
+    drawDigit d4 world 18 9
+
+    world[(11*24) +  11] <- true
+    world[(13*24) + 11] <- true
+
+    world
+
+
+let getNext (world: World): World=
+    let nextWorld = Array.create 576 false
+    for i in 0..575 do
+        let tl = if i > 24 && i % 24 > 0 then Some world[i - 25] else None
+        let t = if i - 24 >= 0 then Some world[i - 24] else None
+        let tr = if i - 23 >= 0 && i % 24 < 23 then Some world[i - 23] else None
+        let l = if i - 1 >= 0 && i % 24 > 0 then Some world[i - 1] else None
+        let r = if i % 24 < 23 then Some world[i + 1] else None
+        let bl = if i + 23 < 576 && i % 24 > 0 then Some world[i + 23] else None
+        let b = if i + 24 < 576 then Some world[i + 24] else None
+        let br = if i + 25 < 576 && i % 24 < 23 then Some world[i + 25] else None
+
+        let aliveNeighbours =
+            [tl; t; tr; l; r; bl; b; br]
+            |> List.choose id
+            |> List.filter id
+            |> List.length
+        let alive =
+            match world[i], aliveNeighbours with
+            | true, 0
+            | true, 1 -> false
+            | true, 2
+            | true, 3 -> true
+            | true, _ -> false
+            | false, 3 -> true
+            | false, _ -> false
+        nextWorld[i] <- alive
+    nextWorld
+
+let life =
+    scene {
+        let! ctx = getCtx()
+        let! worldState = useState { getInitialWorld ctx.now.Hour ctx.now.Minute }
+
+        let! minChanged = Trigger.valueChanged ctx.now.Minute
+        let! frameTrigger = Trigger.valueChanged (ctx.now.Millisecond / 500)
+        let world =
+            if minChanged then
+                let world = getInitialWorld ctx.now.Hour ctx.now.Minute
+                do worldState.value <- world
+                world
+            else if frameTrigger then
+                let world = worldState.value
+                let nextWorld = getNext world
+                do worldState.value <- nextWorld
+                nextWorld
+            else
+                worldState.value
+        for r in 0..23 do
+            for c in 0..23 do
+                let color =
+                    if world[(r * 24) + c] then Color.rgb(100, 00, 00) else Color.rgb(50, 50, 50)
+                pxl.xy(c,r).stroke(color)
+    }
+
+let diffuser =
+    scene {
+        rect.xywh(0, 07, 24, 9).fill(Color.argb(80, 0, 0, 0)).useAntiAlias()
+        rect.xywh(0, 08, 24, 7).fill(Color.argb(80, 0, 0, 0)).useAntiAlias()
+    }
+
+let all =
+    scene {
+        let! ctx = getCtx ()
+        life
+        //diffuser
+        time ctx.now
+    }
+
+all |> Simulator.start
+
+(*
+Simulator.stop ()
+*)
---
+++ PxlApps/04_Submissions/ursenzler_NicosDog.fsx
@@ -0,0 +1,117 @@
+﻿#load "../../.deps/PxlLocalDevShadow.fsx"
+
+open PxlLocalDevShadow
+
+open System
+open Pxl
+open Pxl.Ui
+
+let time (now: DateTime) =
+    scene {
+        let! ctx = getCtx ()
+
+        let timeText =
+            text.var4x5($"%02d{now.Hour}:%02d{now.Minute}").color (Colors.white)
+
+        let textWidth = timeText.measure ()
+        let marginLeft = (ctx.width - textWidth) / 2
+
+        timeText
+            .xy(marginLeft, 4)
+            .color Colors.white
+    }
+
+let image =
+    [
+        "                               yyyyyyyy           g                                 "
+        "                                   yyyyyyyy    ggggg                                "
+        "                               yyyyyyyy       gggggggg                              "
+        "                                   yyyyyyyy     ggg ggg                             "
+        "                                    y y          gg  g                              "
+        "                                    y y     e e                                     "
+        "                                    y y   e eeee                                    "
+        "                                    y y  eeeeeeee                                   "
+        "                                          eeeeee                                    "
+        "                                         eeeeeeee                                   "
+        "                                          e be e                                    "
+        "                                            bb      lll lll                         "
+        "                                            bb      lfl lfl                         "
+        "                                            bb      lfl lfl                         "
+        "                                            bb      ddddddd                         "
+        "                                            bb      dcdddcd                         "
+        "                                            bb      ddddddd                         "
+        "                                            bb      ddcccdd                         "
+        "                                            bb       ddddd                          "
+        "                                            bb       ddddd                          "
+        "                                    tdddddddddddddddddddddd                         "
+        "                                   ttdddddddddddddddddddddd                         "
+        "                                   t dd dd  bb        dd dd                         "
+        "                                     dd dd  bb        dd dd                         "
+
+
+    ]
+
+let shift i =
+    let shiftLine (line: string) =
+        line.AsSpan().Slice(line.Length - 1 - i - 24, 24).ToString()
+    image
+    |> List.map shiftLine
+
+let dog (minute: int) (second: int) =
+    scene {
+        //let! trigger = Trigger.valueChanged(second)
+        pxl.xy(second % 24, 1).stroke(Colors.blue)
+
+        //if trigger then
+        let current = shift second
+
+        let pixels =
+            [|0..23|]
+            |> Array.collect (fun l ->
+                current[l]
+                |> Seq.toArray
+                |> Array.mapi (fun i pixel -> l, i, pixel))
+
+        for l, c, pixel in pixels do
+            let color =
+                match pixel with
+                | 'b' -> Colors.brown
+                | 'y' -> Colors.yellow
+                | 'd' -> Colors.saddleBrown
+                | 'e' -> Colors.green
+                | 'g' -> Colors.gray
+                | 't' -> Colors.rosyBrown
+                | 'l' -> Colors.saddleBrown
+                | 'f' -> Colors.sandyBrown
+                | 'c' -> Colors.bisque
+                | _ when l <= 20 -> Colors.blue
+                | _ -> Colors.beige
+            pxl.xy(c,l).stroke(color)
+    }
+
+let diffuser =
+    scene {
+        rect
+            .xywh(0, 2, 24, 9)
+            .fill(Color.argb (80, 0, 0, 0))
+            .useAntiAlias ()
+
+        rect
+            .xywh(0, 3, 24, 7)
+            .fill(Color.argb (80, 0, 0, 0))
+            .useAntiAlias ()
+    }
+
+let all =
+    scene {
+        let! ctx = getCtx ()
+        dog ctx.now.Minute ctx.now.Second
+        diffuser
+        time ctx.now
+    }
+
+all |> Simulator.start
+(*
+Simulator.stop ()
+*)
+
